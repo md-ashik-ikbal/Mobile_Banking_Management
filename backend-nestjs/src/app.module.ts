8ce200199e2447d70260da3837263e6158fe0450
MD. ASHIK IKBAL <mdashikikbal904@gmail.com>
2024-12-16 18:01:17 +0600

    Updated database

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CustomerModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.env'
+        }),
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres', 
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZATION'),
+        autoLoadEntities: true,
+        logging: true
+      }),
+      inject: [ConfigService],
+    }),
+    CustomerModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
