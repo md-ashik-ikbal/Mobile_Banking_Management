ad764ecca3c7253e1ad7fa3a95a7404d6d95711f
MD. ASHIK IKBAL <mdashikikbal904@gmail.com>
2024-12-16 18:08:40 +0600

    Added customer auth

--- backend-nestjs/src/customer/customer.module.ts
+++ backend-nestjs/src/customer/customer.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [CustomerController],
   providers: [CustomerService],
+  imports: [AuthModule],
 })
 export class CustomerModule {}
