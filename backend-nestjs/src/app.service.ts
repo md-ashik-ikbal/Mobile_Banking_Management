52fee8c98a581ee65c8e6a4e316ca8bd637ad6b9
MD. ASHIK IKBAL <mdashikikbal904@gmail.com>
2024-12-17 18:14:43 +0600

    Updated root URL

--- backend-nestjs/src/app.service.ts
+++ backend-nestjs/src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+      <body style="margin: 0; background-color: #111827; color: #cbd5e1">
+        <div style="height: 100vh; display: flex; justify-content: center; align-items: center;">
+          <h1 style="margin: 0;">The Application Is Working</h1>
+        </div>
+      </body>
+    `;
   }
 }
