d5f25bae94417962fd023772d3dd49b8be2763cf
MD. ASHIK IKBAL <mdashikikbal904@gmail.com>
2024-12-17 12:47:14 +0600

    Updated auth

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -27,7 +28,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
-    CustomerModule
+    CustomerModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend-nestjs/src/customer/customer.module.ts
+++ backend-nestjs/src/customer/customer.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [CustomerController],
   providers: [CustomerService],
-  imports: [AuthModule],
+  imports: [],
 })
 export class CustomerModule {}
